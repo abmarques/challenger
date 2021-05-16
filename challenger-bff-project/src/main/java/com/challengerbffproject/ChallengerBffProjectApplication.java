26e34bf7f50f9b34dfbe8a868ec0dc3b9bc57857
unknown <abmarques.si@gmail.com>
2021-05-15 22:31:55 -0300

    BFF Project creation

---
+++ challenger-bff-project/src/main/java/com/challengerbffproject/ChallengerBffProjectApplication.java
@@ -0,0 +1,13 @@
+package com.challengerbffproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengerBffProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengerBffProjectApplication.class, args);
+	}
+
+}
---
+++ challenger-bff-project/src/test/java/com/challengerbffproject/ChallengerBffProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.challengerbffproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengerBffProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
